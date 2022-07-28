bda63c87a3e715515202844da8c8706ee0ffd89c
Shane Melton <smelton@bitwarden.com>
2022-07-28 11:11:09 -0700

    Remove unnecessary organization lookup

--- apps/web/src/app/modules/organizations/billing/organization-billing-tab.component.ts
+++ apps/web/src/app/modules/organizations/billing/organization-billing-tab.component.ts
@@ -1,21 +1,7 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-
-import { OrganizationService } from "@bitwarden/common/abstractions/organization.service";
-import { Organization } from "@bitwarden/common/models/domain/organization";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-org-billing-tab",
   templateUrl: "organization-billing-tab.component.html",
 })
-export class OrganizationBillingTabComponent implements OnInit {
-  organization: Organization;
-
-  constructor(private route: ActivatedRoute, private organizationService: OrganizationService) {}
-
-  ngOnInit() {
-    this.route.parent.params.subscribe(async (params) => {
-      this.organization = await this.organizationService.get(params.organizationId);
-    });
-  }
-}
+export class OrganizationBillingTabComponent {}
