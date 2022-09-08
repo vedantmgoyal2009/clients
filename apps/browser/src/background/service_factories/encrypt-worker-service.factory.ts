468a825680867d54e13c33e4c6f9c71b514284f8
Thomas Rittson <31796059+eliykat@users.noreply.github.com>
2022-09-09 07:34:10 +1000

    Fix cache name of factory
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>

--- apps/browser/src/background/service_factories/encrypt-worker-service.factory.ts
+++ apps/browser/src/background/service_factories/encrypt-worker-service.factory.ts
@@ -22,7 +22,7 @@ export function EncryptWorkerServiceFactory(
 ): Promise<AbstractEncryptWorkerService> {
   return factory(
     cache,
-    "vaultTimeoutService",
+    "encryptWorkerService",
     opts,
     async () =>
       new EncryptWorkerService(
