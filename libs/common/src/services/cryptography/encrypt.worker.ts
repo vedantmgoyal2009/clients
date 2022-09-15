5bb6535f0a4ec251a87e2de09334945a38b8a719
Thomas Rittson <trittson@bitwarden.com>
2022-09-15 15:35:29 +1000

    Use helper functions in worker

--- libs/common/src/services/cryptography/encrypt.worker.ts
+++ libs/common/src/services/cryptography/encrypt.worker.ts
@@ -42,19 +42,8 @@ workerApi.addEventListener("message", async (event: { data: string }) => {
     keys: [string, Jsonify<SymmetricCryptoKey>][] | Jsonify<SymmetricCryptoKey>;
   } = JSON.parse(event.data);
 
-  let keys: any;
-  if (request.keys instanceof Array) {
-    keys = new Map(
-      request.keys.map(([orgId, jsonKey]) => [orgId, SymmetricCryptoKey.fromJSON(jsonKey)])
-    );
-  } else {
-    keys = SymmetricCryptoKey.fromJSON(request.keys);
-  }
-
-  const items: IDecryptable<any>[] = request.items.map((i) => {
-    const itemClass = getClass(i.typeName);
-    return itemClass.fromJSON(i);
-  });
+  const keys = buildKeys(request.keys);
+  const items = request.items.map(buildItem);
 
   const result = await encryptService.decryptItems(items, keys);
 
@@ -63,3 +52,20 @@ workerApi.addEventListener("message", async (event: { data: string }) => {
     items: JSON.stringify(result),
   });
 });
+
+function buildKeys(keys: [string, Jsonify<SymmetricCryptoKey>][] | Jsonify<SymmetricCryptoKey>) {
+  if (keys == null) {
+    return null;
+  }
+
+  if (keys instanceof Array) {
+    return new Map(keys.map(([orgId, jsonKey]) => [orgId, SymmetricCryptoKey.fromJSON(jsonKey)]));
+  }
+
+  return SymmetricCryptoKey.fromJSON(keys);
+}
+
+function buildItem(jsonItem: Jsonify<IDecryptable<any>>): IDecryptable<any> {
+  const itemClass = getClass(jsonItem.typeName);
+  return itemClass.fromJSON(jsonItem);
+}
