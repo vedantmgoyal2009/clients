f300fbf37f6edd5f367c169b82ad059321c75e7d
Thomas Rittson <trittson@bitwarden.com>
2022-09-21 14:49:54 +1000

    Remove unused interface property

--- libs/common/src/interfaces/IDecryptable.ts
+++ libs/common/src/interfaces/IDecryptable.ts
@@ -3,6 +3,5 @@ import { InitializerKey } from "../services/cryptography/classInitializers";
 
 export interface IDecryptable<T> {
   initializerKey: InitializerKey;
-  organizationId: string;
   decrypt: (key?: SymmetricCryptoKey) => Promise<T>;
 }
