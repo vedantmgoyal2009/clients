fd5d76559093f2f3d268730faecd3ebb96117497
Rui Tome <rtome@bitwarden.com>
2022-09-29 17:56:26 +0100

    [EC-584] Removed ListResponse from OrganizationExportResponse properties

--- libs/common/src/models/response/organizationExportResponse.ts
+++ libs/common/src/models/response/organizationExportResponse.ts
@@ -1,21 +1,20 @@
 import { BaseResponse } from "./baseResponse";
 import { CipherResponse } from "./cipherResponse";
 import { CollectionResponse } from "./collectionResponse";
-import { ListResponse } from "./listResponse";
 
 export class OrganizationExportResponse extends BaseResponse {
-  collections: ListResponse<CollectionResponse>;
-  ciphers: ListResponse<CipherResponse>;
+  collections: CollectionResponse[];
+  ciphers: CipherResponse[];
 
   constructor(response: any) {
     super(response);
     const collections = this.getResponseProperty("Collections");
     if (collections != null) {
-      this.collections = new ListResponse(collections, CollectionResponse);
+      this.collections = collections.map((c: any) => new CollectionResponse(c));
     }
     const ciphers = this.getResponseProperty("Ciphers");
     if (ciphers != null) {
-      this.ciphers = new ListResponse(ciphers, CipherResponse);
+      this.ciphers = ciphers.map((c: any) => new CipherResponse(c));
     }
   }
 }
