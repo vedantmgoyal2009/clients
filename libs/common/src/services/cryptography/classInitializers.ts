566dc64dd4016be8014948ec2e570bac7aee847b
Thomas Rittson <trittson@bitwarden.com>
2022-10-06 13:35:50 +1000

    Improve typing on getInitializer

--- libs/common/src/services/cryptography/classInitializers.ts
+++ libs/common/src/services/cryptography/classInitializers.ts
@@ -9,11 +9,11 @@ import { InitializerKey } from "./initializerKey";
  * Internal reference of classes so we can reconstruct objects properly.
  * Each entry should be keyed using the IDecryptable.initializerKey property
  */
-const classInitializers = {
+const classInitializers: Record<InitializerKey, (obj: any) => any> = {
   [InitializerKey.Cipher]: Cipher.fromJSON,
   [InitializerKey.CipherView]: CipherView.fromJSON,
 };
 
-export function getInitializer<T>(className: InitializerKey) {
-  return classInitializers[className] as unknown as (obj: Jsonify<T>) => T;
+export function getInitializer<T>(className: InitializerKey): (obj: Jsonify<T>) => T {
+  return classInitializers[className];
 }
