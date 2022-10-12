882b037d848f888ac686077af18c747ce97fdffd
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-10-12 09:09:09 +0200

    [EC-63] feat: hide overflow and show ellipsis

--- apps/web/src/app/shared/components/breadcrumbs/breadcrumbs.module.ts
+++ apps/web/src/app/shared/components/breadcrumbs/breadcrumbs.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 
+import { LinkModule } from "@bitwarden/components";
+
 import { BreadcrumbComponent } from "./breadcrumb.component";
 import { BreadcrumbsComponent } from "./breadcrumbs.component";
 
 @NgModule({
+  imports: [LinkModule],
   declarations: [BreadcrumbsComponent, BreadcrumbComponent],
+  exports: [BreadcrumbsComponent, BreadcrumbComponent],
 })
 export class BreadcrumbsModule {}
