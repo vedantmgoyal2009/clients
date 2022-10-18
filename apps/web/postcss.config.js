490b945a0fd61560d5a59f211c4e6ec3ca2d0948
Hinton <hinton@users.noreply.github.com>
2022-10-18 13:24:35 +0200

    Change search icon to use the new bit icon

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
 module.exports = {
-  plugins: [require("tailwindcss/nesting"), require("tailwindcss"), require("autoprefixer")],
+  plugins: [require("tailwindcss"), require("autoprefixer"), require("postcss-nested")],
 };
