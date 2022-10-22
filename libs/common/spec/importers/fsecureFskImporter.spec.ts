471ad1c0768acf5d135ab327a4484beaf62aa36e
Daniel James Smith <djsmith@web.de>
2022-10-22 15:09:50 +0200

    Add new testdata and rewrite existing tests

--- libs/common/spec/importers/fsecureFskImporter.spec.ts
+++ libs/common/spec/importers/fsecureFskImporter.spec.ts
@@ -1,77 +1,50 @@
+import { CipherType } from "@bitwarden/common/enums/cipherType";
 import { FSecureFskImporter as Importer } from "@bitwarden/common/importers/fsecureImporters/fsecureFskImporter";
 
-const TestDataWithStyleSetToWebsite: string = JSON.stringify({
-  data: {
-    "8d58b5cf252dd06fbd98f5289e918ab1": {
-      color: "#00baff",
-      reatedDate: 1609302913,
-      creditCvv: "",
-      creditExpiry: "",
-      creditNumber: "",
-      favorite: 0,
-      modifiedDate: 1609302913,
-      notes: "note",
-      password: "word",
-      passwordList: [],
-      passwordModifiedDate: 1609302913,
-      rev: 1,
-      service: "My first pass",
-      style: "website",
-      type: 1,
-      url: "https://bitwarden.com",
-      username: "pass",
-    },
-  },
-});
-
-const TestDataWithStyleSetToGlobe: string = JSON.stringify({
-  data: {
-    "8d58b5cf252dd06fbd98f5289e918ab1": {
-      color: "#00baff",
-      reatedDate: 1609302913,
-      creditCvv: "",
-      creditExpiry: "",
-      creditNumber: "",
-      favorite: 0,
-      modifiedDate: 1609302913,
-      notes: "note",
-      password: "word",
-      passwordList: [],
-      passwordModifiedDate: 1609302913,
-      rev: 1,
-      service: "My first pass",
-      style: "globe",
-      type: 1,
-      url: "https://bitwarden.com",
-      username: "pass",
-    },
-  },
-});
+import { CreditCardTestEntry, LoginTestEntry } from "./testData/fsecureFsk/testData";
 
 describe("FSecure FSK Importer", () => {
-  it("should parse data with style set to website", async () => {
+  it("should import data of type login", async () => {
     const importer = new Importer();
-    const result = await importer.parse(TestDataWithStyleSetToWebsite);
+    const LoginTestEntryStringified = JSON.stringify(LoginTestEntry);
+    const result = await importer.parse(LoginTestEntryStringified);
     expect(result != null).toBe(true);
 
     const cipher = result.ciphers.shift();
-    expect(cipher.login.username).toEqual("pass");
-    expect(cipher.login.password).toEqual("word");
+
+    expect(cipher.name).toEqual("example.com");
+    expect(cipher.notes).toEqual("some note for example.com");
+
+    expect(cipher.type).toBe(CipherType.Login);
+    expect(cipher.login.username).toEqual("jdoe");
+    expect(cipher.login.password).toEqual("somePassword");
+
     expect(cipher.login.uris.length).toEqual(1);
     const uriView = cipher.login.uris.shift();
-    expect(uriView.uri).toEqual("https://bitwarden.com");
+    expect(uriView.uri).toEqual("https://www.example.com");
   });
 
-  it("should parse data with style set to globe", async () => {
+  it("should import data of type creditCard", async () => {
     const importer = new Importer();
-    const result = await importer.parse(TestDataWithStyleSetToGlobe);
+    const CreditCardTestEntryStringified = JSON.stringify(CreditCardTestEntry);
+    const result = await importer.parse(CreditCardTestEntryStringified);
     expect(result != null).toBe(true);
 
     const cipher = result.ciphers.shift();
-    expect(cipher.login.username).toEqual("pass");
-    expect(cipher.login.password).toEqual("word");
-    expect(cipher.login.uris.length).toEqual(1);
-    const uriView = cipher.login.uris.shift();
-    expect(uriView.uri).toEqual("https://bitwarden.com");
+
+    expect(cipher.name).toEqual("My credit card");
+    expect(cipher.notes).toEqual("some notes to my card");
+
+    expect(cipher.type).toBe(CipherType.Card);
+    expect(cipher.card.cardholderName).toEqual("John Doe");
+    expect(cipher.card.number).toEqual("4242424242424242");
+    expect(cipher.card.code).toEqual("123");
+
+    expect(cipher.fields.length).toBe(2);
+    expect(cipher.fields[0].name).toEqual("Expiration");
+    expect(cipher.fields[0].value).toEqual("22.10.2026");
+
+    expect(cipher.fields[1].name).toEqual("PIN");
+    expect(cipher.fields[1].value).toEqual("1234");
   });
 });
